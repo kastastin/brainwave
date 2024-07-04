b959913f976e31a2f8caca80ed30a5814df3f264
kastastin <kostya242421@gmail.com>
2024-07-04 06:28:39 +0300

    added Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
+import Header from "./components/Header";
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import Button from "./components/Button";
 
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-
       <div className="overflow-hidden pt-[4.75rem] lg:pt-[5.25rem]">
-        <Button href="#login" className="mt-10">
-          Something
-        </Button>
+        <Header />
       </div>
 
       <ButtonGradient />
